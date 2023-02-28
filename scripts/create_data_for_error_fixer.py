20f9fd26cebd415a84f98abc0a9bb84617394ff3
Kristina Szabova <k.szabova98@gmail.com>
2023-02-28 17:57:37 +0100

    Creation of datasets with removal of syntactic subtrees

--- scripts/create_data_for_error_fixer.py
+++ scripts/create_data_for_error_fixer.py
@@ -1,4 +1,5 @@
 import argparse
+import datasets
 
 from utils.data_mangling import *
 
@@ -9,6 +10,10 @@ def get_argparser():
     parser.add_argument("--split", type=str, default="train")
     parser.add_argument("--dest_file", type=str, default="data/error_fixer_data.data")
     parser.add_argument("--include_concept_completeness", action="store_true")
+    parser.add_argument("--deletion", action="store_true")
+    parser.add_argument("--replacement", action="store_true")
+    parser.add_argument("--subtree_deletion", action="store_true")
+    parser.add_argument("--keep_original", action="store_true")
     return parser
 
 
@@ -19,22 +24,36 @@ def main():
     nlp = None
     # nlp must be instantiated if we want a column determining
     # whether the "output" has all input concepts present
-    if args.include_concept_completeness:
+    if args.include_concept_completeness or args.subtree_deletion:
         nlp = get_nlp_object()
 
+    ds_list = []
     data_orig = get_data(
-        args.dataset, args.split, args.include_concept_completeness, nlp=nlp
+        args.dataset,
+        args.split,
+        args.include_concept_completeness or args.subtree_deletion,
+        nlp=nlp,
     )
     vocab = gather_vocab(data_orig)
-    data_with_replacement = get_dataset_with_replacement(
-        data_orig, vocab, args.include_concept_completeness, nlp=nlp
-    )
-    data_with_deletion = get_dataset_with_deletion(
-        data_orig, args.include_concept_completeness, nlp=nlp
-    )
-    data = datasets.concatenate_datasets(
-        [data_orig, data_with_replacement, data_with_deletion]
-    )
+
+    if args.keep_original:
+        ds_list.append(data_orig)
+    if args.replacement:
+        data_with_replacement = get_dataset_with_replacement(
+            data_orig, vocab, args.include_concept_completeness, nlp=nlp
+        )
+        ds_list.append(data_with_replacement)
+    if args.deletion:
+        data_with_deletion = get_dataset_with_deletion(
+            data_orig, args.include_concept_completeness, nlp=nlp
+        )
+        ds_list.append(data_with_deletion)
+    if args.subtree_deletion:
+        data_with_subtree_deletion = get_dataset_with_subtree_deletion(
+            data_orig, args.include_concept_completeness, nlp=nlp
+        )
+        ds_list.append(data_with_subtree_deletion)
+    data = datasets.concatenate_datasets(ds_list)
     data = data.train_test_split(test_size=0.1)
     data.save_to_disk(args.dest_file)
 
